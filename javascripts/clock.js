00550a5e87fa796a59a23f65b3fb36c97ca6b19c
David Sarma <davidsarma@gmail.com>
2013-10-31 15:20:18 -0700

    Created clock.js

---
+++ javascripts/clock.js
@@ -0,0 +1,11 @@
+var canvas = document.getElementById('canvas'),
+	context = canvas.getContext('2d'),
+    FONT_HEIGHT = 15,
+    MARGIN = 35,
+    HAND_TRUNCATION = canvas.width/25,
+    HOUR_HAND_TRUNCATION = canvas.width/10,
+    NUMERAL_SPACING = 20,
+    RADIUS = canvas.width/2 - MARGIN,
+    HAND_RADIUS = RADIUS + NUMERAL_SPACING;
+    
+//Functions.........

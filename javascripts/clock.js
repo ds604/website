2dcf53cf85df81b2e565876cc4ff2448d45a5c83
David Sarma <davidsarma@gmail.com>
2013-11-01 01:22:04 -0700

    Updated clock.js

--- javascripts/clock.js
+++ javascripts/clock.js
@@ -37,11 +37,38 @@ function drawCenter() {
 }
 
 function drawHand(loc, isHour) {
-	
+	var angle = (Math.PI*2) * (loc/60) - Math.PI/2,
+        handRadius = isHour ? RADIUS - HAND_TRUNCATION - HOUR_HAND_TRUNCATION
+    						: RADIUS - HAND_TRUNCATION;
+  	
+  	context.moveTo(canvas.width/2, canvas.height/2);
+  	context.lineTo(canvas.width/2 + Math.cos(angle)*handRadius,
+                   canvas.height/2 + Math.sin(angle)*handRadius);
+  	context.stroke();
 }
 
-function drawHands() {}
+function drawHands() {
+	var date = new Date,
+        hour = date.getHours();
+  
+  hour = hour > 12 ? hour - 12 : hour;
+  
+  drawHand(hour*5 + (date.getMinutes()/60)*5, true, 0.5);
+  drawHand(date.getMinutes(), false, 0.5);
+  drawHand(date.getSeconds(), false, 0.2);
+}
 
-function drawClock() {}
+function drawClock() {
+
+  context.clearRect(0, 0, canvas.width, canvas.height);
+  
+  drawCircle();
+  drawCenter();
+  drawHands();
+  drawNumerals();
+}
 
 //Initialization...........
+
+context.font = FONT_HEIGHT + 'px Arial';
+loop = setInterval(drawClock, 1000);

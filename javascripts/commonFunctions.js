ae5fa91555b74346ac612663439920671393dda8
David Sarma <davidsarma@gmail.com>
2014-03-03 06:45:03 -0500

    Create commonFunctions.js

---
+++ javascripts/commonFunctions.js
@@ -0,0 +1,58 @@
+/////////////////////////////////////////////////////
+//Create array of numbers between start and stop
+linspace = function(start, stop, divisions){
+        start = start || 0;
+        stop  = stop || 1;
+        divisions = divisions || 100;
+        
+        return 
+    };
+
+/////////////////////////////////////////////////////
+//Linear interpolation of 'amount' values between start and stop
+lerp = function(start,stop,amount) {
+        start = start || 0;
+        stop = stop || 1;
+        amount = amount || 0.5;
+        
+        return (1-amount) * start + amount * stop;
+    };
+    
+cos = function(x, amplitude, frequency, phase, offset){
+        amplitude = amplitude || 1;
+        frequency = frequency || 
+        
+        return amplitude * (offset + Math.cos(frequency * (x + phase)));
+    };
+    
+/////////////////////////////////////////////////////
+/////////////////////////////////////////////////////
+//Add two arrays
+//
+//var a = [1,2,3];
+//var b = [9,2,7];
+//
+//   // pass an Array
+//var c = a.add( b );  // [10,4,10]
+//
+//   // pass a number
+//var d = a.add( 5 );  // [6,7,8]
+
+Array.prototype.add = function( b ) {
+    var a = this,
+        c = [];
+    if( Object.prototype.toString.call( b ) === '[object Array]' ) {
+        if( a.length !== b.length ) {
+            throw "Array lengths do not match.";
+        } else {
+            for( var i = 0; i < a.length; i++ ) {
+                c[ i ] = a[ i ] + b[ i ];
+            }
+        }
+    } else if( typeof b === 'number' ) {
+        for( var i = 0; i < a.length; i++ ) {
+            c[ i ] = a[ i ] + b;
+        }
+    }
+    return c;
+};

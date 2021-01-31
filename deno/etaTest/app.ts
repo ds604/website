fd7c0aa5eb04337b087aea386e32e05d89c41fa5
David Sarma <davidsarma@gmail.com>
2021-01-31 04:48:07 -0500

    Update app.ts

--- deno/etaTest/app.ts
+++ deno/etaTest/app.ts
@@ -1,20 +1,20 @@
 import { renderFile, configure } from "https://deno.land/x/eta@v1.11.0/mod.ts"
 
 // const viewPath = `${Deno.cwd()}/views/`
-const viewPath = `./views/`
+// const viewPath = `./views/`
 
-// Set Eta's configuration
-configure({
-  // This tells Eta to look for templates
-  // In the /views directory
-  views: viewPath
-})
+// // Set Eta's configuration
+// configure({
+//   // This tells Eta to look for templates
+//   // In the /views directory
+//   views: viewPath
+// })
 
 // Eta assumes the .eta extension if you don't specify an extension
 // You could also write renderFile("template.eta"),
 // renderFile("/template"), etc.
 
-let templateResult = await renderFile("./template", { food: "cake" })
+let templateResult = await renderFile("./views/template.eta", { food: "cake" })
 
 console.log(templateResult)
 /*
